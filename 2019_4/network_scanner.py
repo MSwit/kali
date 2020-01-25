b375f798f61e102fc632a63c71bc7558de9412ff
Maik Switalski <Maik.Switalski@gmx.de>
2020-01-25 11:24:56 +0100

    improve network scanner

--- 2019_4/network_scanner.py
+++ 2019_4/network_scanner.py
@@ -4,7 +4,26 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=3, verbose=True)[0]
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
 
-scan("192.168.178.1/24")
+def print_result(results_list):
+
+    print("IP\t\t\tMAC Address\n-------------------------------------------")
+    for client in results_list:
+        print(client['ip'] + "\t\t" + client['mac'])
+
+
+scan_result = scan("192.168.178.1/24")
+print_result(scan_result)
