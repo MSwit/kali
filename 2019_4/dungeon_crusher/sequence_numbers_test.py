9b2226373066b55a3d2cce703394baea761b6064
Maik Switalski <maik@zauberzeug.com>
2020-07-04 15:25:55 +0200

    [dungeon crusher] add script to find out how the increase of sequence_number works

---
+++ 2019_4/dungeon_crusher/sequence_numbers_test.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+from mitmproxy import http
+from mitmproxy import ctx
+import json
+
+
+do_it = True
+
+find_boss_for_siege_request_flow = None
+sequence_numbers = {}
+current_kind = ""
+
+last_sequence_number = None
+last_seq_num = None
+
+
+def update_seq_numbers(json_content):
+    return json_content
+    content = json.loads(json.dumps(json_content))
+    if "sequence_number" in content:
+        sequence_number = content['sequence_number']
+        if "seq_num" in content:
+            seq_num = content['seq_num']
+
+            max_seq_num = sequence_numbers.get(str(sequence_number))
+            if max_seq_num > seq_num:
+                seq_num = max_seq_num + 1
+            sequence_numbers[str(sequence_number)] = seq_num
+            content['seq_num'] = seq_num
+    return content
+
+
+def request(flow: http.HTTPFlow) -> None:
+    global find_boss_for_siege_request_flow
+    global sequence_numbers
+
+    global last_sequence_number
+    global last_seq_num
+
+    url = flow.request.pretty_url
+    if not url.startswith("https://soulhunters"):
+        flow.kill()
+        return
+
+    try:
+        ctx.log.error("------- request --------")
+        if len(flow.request.get_content()) == 0:
+            ctx.log.error(
+                f" Request without a body. Content length = 0. Method was {flow.request.method}")
+            return
+        json_content_list = json.loads(flow.request.get_content())
+
+        if type(json_content_list) is dict:
+            ctx.log.error(
+                "This request has a dict as content: So it seems unintersting")
+            ctx.log.error(json.dumps(json_content_list, indent=2))
+            return
+
+        ctx.log.error(
+            f"there are {len(json_content_list)} array elements in this request.\n")
+        for json_content in json_content_list:
+
+            ctx.log.error(str(json_content))
+
+            try:
+                sequence_number = json_content['sequence_number']
+                seq_num = json_content['seq_num']
+                kind = json_content['kind']
+                if last_sequence_number is not None and last_seq_num is not None:
+
+                    expected_sequence_number = last_sequence_number
+                    expected_seq_num = last_seq_num + 1
+                    ctx.log.error(f"Kind : {kind}")
+                    if kind in "mob_reward_consumed":
+                        ctx.log.error(
+                            "mob_reward_consumed detected; increasing sequence_number")
+                        expected_sequence_number = last_sequence_number + 1
+                    if kind in 'state_updated':
+                        ctx.log.error(
+                            "state_updated detected; increasing sequence_number")
+                        expected_sequence_number = last_sequence_number + 1
+
+                    if sequence_number != expected_sequence_number:
+                        ctx.log.error(
+                            "[-] Error: expected sequence_number wrong.")
+                        ctx.log.error(
+                            f"[-]  expected : {expected_sequence_number}, but was {sequence_number}")
+                        exit(1)
+
+                    if seq_num != expected_seq_num:
+                        ctx.log.error("[-] Error: expected seq_num wrong.")
+                        ctx.log.error(
+                            f"[-]  expected : {expected_seq_num}, but was {seq_num}")
+                        exit(1)
+                ctx.log.error(
+                    f"setting sequence_number: {sequence_number} and seq_num: {seq_num}")
+                last_sequence_number = sequence_number
+                last_seq_num = seq_num
+
+            except Exception as e:
+                ctx.log.error("Error during printing seq numbers \n" + str(e))
+
+    except Exception as e:
+        ctx.log.error(f"[-] Error: {str(e)}")
+        content = json.loads(flow.request.get_content())
+        ctx.log.error(str(type(content)))
+        ctx.log.error(f"[-] {json.dumps(content,indent=2)}")
+
+        exit(1)
+
+
+def response(flow: http.HTTPFlow) -> None:
+
+    ctx.log.error("------------ resonse -------------")
+    flow.kill()
