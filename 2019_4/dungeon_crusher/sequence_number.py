c5814ccb30a75f648252abbd37ebff0d5dfc9ae0
Maik Switalski <maik@zauberzeug.com>
2020-07-10 19:18:59 +0200

    [dungeon crusher, sequence number] reduce jsons. clone jsons, copy.copy only does shallow copy.

--- 2019_4/dungeon_crusher/sequence_number.py
+++ 2019_4/dungeon_crusher/sequence_number.py
@@ -3,6 +3,7 @@
 from mitmproxy import http
 from mitmproxy import ctx
 import json
+import copy
 
 
 class Sequence_Number:
@@ -12,12 +13,13 @@ class Sequence_Number:
         self.last_seq_num = None
 
     def generate_updated_json_list(self, json_content_list):
+        content = json.loads(json.dumps(json_content_list))
         for i, value in enumerate(json_content_list):
-            json_content_list[i] = self.update(value)
+            content[i] = self.generate_updated_json(value)
+        return content
 
     def generate_updated_json(self, json_content):
-        import copy
-        content = copy.copy(json_content)
+        content = json.loads(json.dumps(json_content))
 
         if self.last_sequence_number == None:
             self.last_sequence_number = content['sequence_number']
@@ -32,6 +34,34 @@ class Sequence_Number:
         if not self.is_interesting_request(flow):
             return
         json_content_list = json.loads(flow.request.get_content())
+
+        updated_content_list = self.generate_updated_json_list(json_content_list)
+        if json_content_list != updated_content_list:
+            ctx.log.error("[-] request was updated. but it shouldn't.")
+            ctx.log.error("[-] Original request content: ")
+            ctx.log.error(
+                f"[-] {json.dumps(Sequence_Number.remove_non_trivial_items_list(json_content_list), indent=2)}")
+
+            ctx.log.error("[-] Updated request content: ")
+            ctx.log.error(
+                f"[-]{json.dumps(Sequence_Number.remove_non_trivial_items_list(updated_content_list), indent=2)}")
+
+    @staticmethod
+    def remove_non_trivial_items_list(orginal_json_list):
+        return [Sequence_Number.remove_non_trivial_items(orginal_json) for orginal_json in orginal_json_list]
+
+    @staticmethod
+    def remove_non_trivial_items(orginal_json):
+        content = copy.copy(orginal_json)
+        for i, key in enumerate(orginal_json):
+            _type = type(orginal_json[key])
+            if _type is list:
+                content[key] = []
+            elif _type is dict:
+                content[key] = {}
+            elif key == "state":
+                content[key] = {}
+        return content
 
     def is_interesting_request(self, flow: http.HTTPFlow):
         url = flow.request.pretty_url
--- 2019_4/dungeon_crusher/test_sequence_number.py
+++ 2019_4/dungeon_crusher/test_sequence_number.py
@@ -20,3 +20,13 @@ def test_second_request_gets_updated_seq_num():
     updated_request2 = updater.generate_updated_json(request)
     assert updated_request2['seq_num'] == 1
     assert updated_request2['sequence_number'] == request['sequence_number']
+
+
+def test_delete_arrays_from_json():
+    content = {"a_property": 12, "b_list_property": [1, 2]}
+    modified_content = Sequence_Number.remove_non_trivial_items(content)
+    assert modified_content == {"a_property": 12, "b_list_property": []}
+
+    content = {"a_property": 12, "b_dict_property": {'dict_property_a': "17", "dict_property_b_list": [1, 2, 3]}}
+    modified_content = Sequence_Number.remove_non_trivial_items(content)
+    assert modified_content == {"a_property": 12, "b_dict_property": {}}
