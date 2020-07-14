0e97ed7bd245844bcde01103b0c1ee6a067fd952
Maik Switalski <maik@zauberzeug.com>
2020-07-14 18:40:09 +0200

    [dungeon crusher, sieges] add some flow snippeds to see which request generates which response.

--- 2019_4/dungeon_crusher/sieges.py
+++ 2019_4/dungeon_crusher/sieges.py
@@ -112,3 +112,22 @@ def response(flow: http.HTTPFlow) -> None:
         ctx.log.error(f"[-] An Error occured: Bad Statuscode:")
         ctx.log.error(json.dumps(json.loads(flow.request.get_content()), indent=2))
         ctx.log.error(json.dumps(json.loads(flow.response.get_content()), indent=2))
+
+# aktueller stand...
+# mofified_sieges_2_onePlus_2_should_found_boss_but_didnt.bak
+
+# oneplus3t findet 20-30 mio boss nicht.
+
+
+# request: "find_boss_for_siege"
+# response: {\"sieges\":[{\"id\":\"95eaef8a-2761-4950-aad2-44421601ece4\",\"current_hp\":17680159,\"expires_at....
+# => attack big boss.
+
+
+# request: https://soulhunters.beyondmars.io/api/boss_sieges/sieges/505600d3-8ce4-4488-bc6c-0c12166db877
+# response: "{\"id\":\"505600d3-8ce4-4488-bc6c-0c12166db877\",\"current_hp\":1971309952,\"expires_at\":\"2020-07-14T16:18:07.288Z\", ... scores: [....]
+
+# "[{\"kind\": \"boss_siege_refill_attack\", \"sequence_number\": 2, \"seq_num\": 3}, {\"siege_id\": \"505600d3-8ce4-4488-bc6c-0c12166db877\", \"power_attack\": false, \"autorestore_is_on\": false, \"kind\": \"boss_siege_attack\"
+# {\"boss_siege_refill_result\":{\"attacks_left\":0,\"refill_at\":\"2020-07-14T16:45:44.971Z\",\"updated_at\":\"..Z\"},\"boss_siege_attack_result\":{\"status\":200,\"siege\":{\"expires_at\":\"...\",\"
+#  current_hp\":1655268692,\"id\":\"505600d3-8ce4-4488-bc6c-0c12166db877\",\"created_at\":\"..\",\"updated_at\":\"..\",\"private\":false,\"total_users\":34,\"top_users\":{\"mvp\":\"3bfeb99d-566e-4491-ac39-21b5f3931acd\",\"finder\":\"e66e5ce2-f994-4bac-b4a1-71c87fc0835c\",\"last_hit\":\"f08f6b49-e1b8-4142-90c4-81a9b7741120\"},\"top_attack_id\":\"ab72aaa2-a1ef-442f-94c7-d9259f2e3b0b\",\"boss_config_id\":216125,
+# \"scores\": [....]
