3f187759b6c2608e7be6c2f65366410153e70dba
Maik Switalski <maik@zauberzeug.com>
2020-07-22 21:39:21 +0200

    [mitm] no need for event_loop ?

--- 2019_4/mitm/nonblocking.py
+++ 2019_4/mitm/nonblocking.py
@@ -3,24 +3,34 @@ from mitmproxy import http
 from mitmproxy import ctx
 import time
 from threading import Lock
+import asyncio
+
+
+# https: // stackoverflow.com/a/287944/4082686
+def log_warn(text):
+    print("\033[94m" + text + "\033[0m")
 
 
 class SomeClass:
     @concurrent  # Remove this and see what happens
     def request(self, flow):
+        # asyncio.set_event_loop(loop)
+        log_warn("------------------ request starts -------------------")
         lock.acquire()
-        print("handle request: %s%s" % (flow.request.host, flow.request.path))
-        time.sleep(5)
+        log_warn("handle request: %s%s" %
+                 (flow.request.host, flow.request.path))
+        # time.sleep(5)
         print("start  request: %s%s" % (flow.request.host, flow.request.path))
+        log_warn("------------------ request ende -------------------")
 
-    # @concurrent
     def response(self, flow: http.HTTPFlow) -> None:
-        ctx.log.warn("------------------ RESPONSE starts -------------------")
-        ctx.log.warn("------------------ RESPONSE ende -------------------")
+        log_warn("------------------ RESPONSE starts -------------------")
         lock.release()
+        log_warn("------------------ RESPONSE ende -------------------")
 
 
 lock = Lock()
+# loop = asyncio.new_event_loop()
 
 addons = [
     SomeClass()
