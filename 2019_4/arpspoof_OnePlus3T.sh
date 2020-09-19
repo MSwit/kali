830954fbba8bef307624319632d4d9b1f86619b1
Maik Switalski <maik@zauberzeug.com>
2020-09-19 19:48:08 +0200

    mitmproxy, arpspoofing: Add check for wifi.

--- 2019_4/arpspoof_OnePlus3T.sh
+++ 2019_4/arpspoof_OnePlus3T.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+if [[ -z $(ifconfig | grep 'inet 192.168.178.33') ]]; then
+    echo "[-] Error: cant find wifi"
+    exit 1
+fi
+echo "alles ok"
 arpspoof -i wlan0 -t 192.168.178.23 -r 192.168.178.1
