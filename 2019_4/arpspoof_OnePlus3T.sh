d77ed7ae579d6b9e144bffa28f6ea9bde6d55cec
Maik Switalski <maik@zauberzeug.com>
2020-11-04 18:45:36 +0100

    dungeon crusher: cleanup and improve arp spoofing

--- 2019_4/arpspoof_OnePlus3T.sh
+++ 2019_4/arpspoof_OnePlus3T.sh
@@ -4,5 +4,6 @@ if [[ -z $(ifconfig | grep 'inet 192.168.178.33') ]]; then
     echo "[-] Error: cant find wifi"
     exit 1
 fi
+./forward_to_8080.sh 
 echo "alles ok"
 arpspoof -i wlan0 -t 192.168.178.23 -r 192.168.178.1
